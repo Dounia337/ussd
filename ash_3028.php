cb312e7b84a6a5378240b293f50f02abf6b4b80c
deubaybe.dounia <deubaybe.dounia@ashesi.edu.gh>
2026-04-10 11:35:02 +0000

    done

--- ash_3028.php
+++ ash_3028.php
@@ -1,24 +1,38 @@
 <?php
- 
+
 header('Content-Type: application/json');
- 
+
 $msisdn = isset($_POST['msisdn']) ? trim($_POST['msisdn']) : '';
+
 $sequenceID = isset($_POST['sequenceID']) ? trim($_POST['sequenceID']) : '';
+
 $data = isset($_POST['data']) ? trim($_POST['data']) : '';
+
 $network = isset($_POST['network']) ? trim($_POST['network']) : '';
+
 $timestamp = date('YmdHis');
+
 $message = '';
-$continueFlag = 0;
- 
-$message = "Welcome to Ashesi University Demo\r\n1. Proceed\r\n2. Exit";
+
 $continueFlag = 0;
 
-echo json_encode([
+$message = "Welcome to Ashesi University Demo\r\n1. Proceed\r\n2. Exit";
+
+$continueFlag = 0;
+
+               echo json_encode([
+
     'msisdn' => (string) $msisdn,
+
     'sequenceID' => (string) $sequenceID,
+
     'timestamp' => (string) $timestamp,
+
     'message' => (string) $message,
+
     'continueFlag' => (int) $continueFlag,
+
 ]);
- 
+
 ?>
+ 
